6d3058dc64b430c8ae6cea09981fc2540b96c4b4
neffedych <nefedov1011@gmail.com>
2022-11-04 15:16:14 -0700

    feat: creating file and writing text in it

---
+++ 02-write-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('node:path');
+const prompt = require('prompt-sync')({sigint: true});
+
+
+const data = prompt('Hey there. Tell us something?');
+let writeFile = fs.createWriteStream(__dirname + '/text.txt', {flags : 'a'}); // creating txt file
+writeFile.write(data);
+
