6b1b7880f648a8c2d086a5aabcc819a174a503ab
neffedych <nefedov1011@gmail.com>
2022-11-05 15:23:50 -0700

    feat: add greetings

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,7 +3,11 @@ const path = require('node:path');
 const prompt = require('prompt-sync')({sigint: true});
 
 
-const data = prompt('Hey there. Tell us something?');
+const data = prompt('Hey there. Tell us something? ');
 let writeFile = fs.createWriteStream(__dirname + '/text.txt', {flags : 'a'}); // creating txt file
 writeFile.write(data);
 
+
+process.on('exit', function (){
+    console.log('See you later!');
+});
